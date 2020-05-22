37b9fc69876ef92acf3b8a0acd4adcffd6aacaf9
Raja Biswas <rajjjabiswas@gmail.com>
2020-05-23 01:31:58 +0800

    stiffness computation added in element class

--- fe_utils/material_model.py
+++ fe_utils/material_model.py
@@ -9,11 +9,25 @@ class MaterialModel(object):
     def __init__(self, name = 'linear_elastic'):
         self.name = name 
     
-    def compute_stress(self):
-        pass
+    def compute_stress(self, strain):
+        C = self.compute_tangent_stiffness()
+        assert strain.shape == (4,1), "incompatible shape for strain"
+        stress = np.matmul(C, strain)
+        return stress
     
-    def compute_tangent_stiffness(self):
-        pass
+    def compute_tangent_stiffness(self, strain = None):
+        
+        C = np.zeros(shape = (4,4))
+        C[0,0] = 1.111111e+09
+        C[1,1] = 1.111111e+09
+        C[0,1] = 2.777778e+08
+        C[1,0] = 2.777778e+08
+        C[2,2] = 4.166667e+08*0.5
+        C[3,3] = 4.166667e+08*0.5
+        C[2,3] = 4.166667e+08*0.5
+        C[3,2] = 4.166667e+08*0.5
+        
+        return C
 
 if __name__ == '__main__':
     pass 
--- fe_utils/q8_element.py
+++ fe_utils/q8_element.py
@@ -12,14 +12,14 @@ class FiniteElementQ8(object):
     '''
     class to implement Q8 finite element behavior
     '''
-    def __init__(self, elem_id, node_ids, df_nodes, gp_order = 2):
+    def __init__(self, elem_id, node_ids, df_nodes, material, gp_order = 2):
         '''
         Function to implement element constructor interface
 
         @params:
         elem_id -> Int:
             unique identifier of the element
-        nodes -> List:
+        node_ids -> List:
             List of node ids maintaining the nodal connectivity sequence
         df_nodes -> pd.DataFrame:
             dataframe containing the node information for current alanlysis
@@ -29,10 +29,13 @@ class FiniteElementQ8(object):
         self.elem_type = 'Q8'
         self.analysis_type = 'PE' # plane strain
         self.elem_id = elem_id
+        self.material = material
         
         assert len(node_ids) == 8, "Error: Q8 element must have exactly 8 nodes"
         self.node_ids = node_ids
         self.node_coords = self._get_node_coords(df_nodes)
+
+        self.global_dof_ids = self._get_global_dof_ids()
 
         self.gp_order = gp_order
         self.ngp = gp_order**2 # total number of gauss points per element
@@ -43,14 +46,26 @@ class FiniteElementQ8(object):
         self.derivative_G = gp_data[2]
         self.jacobian_G = gp_data[3]
         self.BM_G = gp_data[4]
+        _, weights = gauss_quadrature(self.gp_order)
+        self.weight_G = weights
 
         # initialize field variables
         self._initialize_sdvs()
+        self. _initialize_last_converged_sdvs()
+
         self._initialize_node_disp()
+        self._initialize_last_converged_node_disp()
+
 
     def __repr__(self):
         return "2-D Q8 element under plane strain"
     
+    def _get_global_dof_ids(self):
+        dofs = []
+        for n in self.node_ids:
+            dofs.extend([n*2, n*2+1])
+        return dofs
+
     def _get_node_coords(self, df_nodes):
         '''
         Function to get coordinates of the nodes for Q8 element
@@ -114,15 +129,46 @@ class FiniteElementQ8(object):
         initialize State Dependant Variables (SDVs) at the gauss points
         the SDVs are: stress tensor, strain tensor
         """
+
         self.stress_G = np.zeros(shape = (4,self.ngp))
         self.strain_G = np.zeros(shape = (4,self.ngp))
+
+    def _initialize_last_converged_sdvs(self):
+        """
+        initialize State Dependant Variables (SDVs) at the gauss points
+        the SDVs are: stress tensor, strain tensor
+        """
+
+        self.stress_G0 = np.zeros(shape = (4,self.ngp))
+        self.strain_G0 = np.zeros(shape = (4,self.ngp))
 
     def _initialize_node_disp(self):
         """
         initialize nodal displacements with zero
         """
-        self.disp_u1 = np.zeros(shape = (8,1))
-        self.disp_u2 = np.zeros(shape = (8,1))
+        self.disp_u = np.zeros(shape = (16,1))
+    
+    def _initialize_last_converged_node_disp(self):
+        """
+        initialize nodal displacements with zero
+        """
+        self.disp_u0 = np.zeros(shape = (16,1))
+
+    def update_stress(self, stress):
+        assert stress.shape == (4,self.ngp), "stress shape mismatch"
+        self.stress_G = stress
+    
+    def update_last_converged_stress(self, stress):
+        assert stress.shape == (4,self.ngp), "stress shape mismatch"
+        self.stress_G0 = stress
+    
+    def update_strain(self, strain):
+        assert strain.shape == (4,self.ngp), "stress shape mismatch"
+        self.strain_G = strain
+
+    def update_last_converged_strain(self, strain):
+        assert strain.shape == (4,self.ngp), "stress shape mismatch"
+        self.strain_G0 = strain
     
     def compute_jacobian(self, dN):
         """
@@ -170,33 +216,74 @@ class FiniteElementQ8(object):
             BM[3, 2*i+1] = derivative_xy[i,0]
         
         return BM
+
+    def compute_response(self, strain):
+        
+        assert strain.shape == (4,4), "shape mismatch for strain"
+        
+        K_elem = np.zeros(shape = (16,16))
+        fint_elem = np.zeros(shape = (16,1))
+        stress = np.zeros(shape = (4, self.ngp))
+        
+        for this_gp in range(self.ngp):
+            J0 = self.jacobian_G[:,:,this_gp]
+            BM = self.BM_G[:,:,this_gp]
+            W = self.weight_G[this_gp]
+            invJ0 = np.linalg.inv(J0)
+            detJ0 = np.linalg.det(J0)
+            factor = W*detJ0
+            this_strain = np.expand_dims(strain[:,this_gp], axis = -1)
+            C_tan = self.material.compute_tangent_stiffness(this_strain)
+            #print(C_tan)
+            K_G = np.matmul((np.matmul(BM.transpose(), C_tan)), BM)
+            K_elem = K_elem + factor * K_G
+            
+            stress_G = self.material.compute_stress(this_strain)
+            stress[:,this_gp] = stress_G.squeeze()
+            fint_G = np.dot(BM.transpose(),stress_G)
+            fint_elem = fint_elem + factor*fint_G
+        
+        return K_elem, fint_elem, stress
+
+            
+
+
     
 
 if __name__ == '__main__':
     
     #sys.path.insert(0, '../')
     from data_loader import MeshDataLoader
+    from material_model import MaterialModel
 
-    filepath = './datasets/one_element_test.xlsx'
-    sheet = 'Sheet1'
-    data_loader = MeshDataLoader(filepath, sheet)
-    df_nodes = data_loader.get_node_data('C10', 'D17')
-    df_elements = data_loader.get_element_data('B19', 'I19')
+    #filepath = './datasets/one_element_test.xlsx'
+    #sheet = 'Sheet1'
+    #data_loader = MeshDataLoader(filepath, sheet)
+    #df_nodes = data_loader.get_node_data('C10', 'D17')
+    #df_elements = data_loader.get_element_data('B19', 'I19')
     #print("node dataframe: ")
     #print(df_nodes)
     #print("element dataframe:" )
     #print(df_elements)
 
+    filepath = './datasets/mesh_data.xlsx'
+    sheet = 'homogenized_localization'
+    data_loader = MeshDataLoader(filepath, sheet)
+    df_nodes = data_loader.get_node_data('C10', 'D157')
+    df_elements = data_loader.get_element_data('B159', 'I197')
 
-    tmp_el = df_elements.sample(1)
-    elem_id = tmp_el.elem_id.values[0]
+
+    #tmp_el = df_elements.sample(1)
+    tmp_el = df_elements.iloc[0,:]
+    elem_id = tmp_el.elem_id
     print('element id = {}'.format(elem_id))
-    node_ids = tmp_el.iloc[0, 1:].values.tolist()
+    node_ids = tmp_el.values[1:]
     print("node ids for the element")
     print(node_ids)
     print('='*10)
+    material = MaterialModel()
 
-    el = FiniteElementQ8(elem_id, node_ids,df_nodes)
+    el = FiniteElementQ8(elem_id, node_ids,df_nodes, material)
     print(el)
     print(el.node_coords)
 
